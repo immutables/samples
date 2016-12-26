211a093f6e626b3999a20f928257b7dd40866541
Eugene Lukash <e.lucash@gmail.com>
2016-12-26 15:30:20 -0800

    update to samples

--- encoding-def/src/encoding/TableEncoding.java
+++ encoding-def/src/encoding/TableEncoding.java
@@ -1,17 +1,42 @@
 package encoding;
 
 import com.google.common.collect.ImmutableTable;
+import com.google.common.collect.Table;
 import org.immutables.encode.Encoding;
 
 @Encoding
-class TableEncoding {
+class TableEncoding<R, C, V> {
   @Encoding.Impl
-  private ImmutableTable<String, String, String> value;
-}
+  private ImmutableTable<R, C, V> value;
 
-/*
-<R, C, V> {
-@Encoding.Impl
-private final ImmutableTable<R, C, V> value = ImmutableTable.of();
+  @Encoding.Expose
+  ImmutableTable<R, C, V> getImmutableTable() {
+    return value;
+  }
+
+  @Encoding.Expose
+  Table<R, C, V> getTable() {
+    return value;
+  }
+
+  @Encoding.Of
+  static <R, C, V> ImmutableTable<R, C, V> init(Table<? extends R, ? extends C, ? extends V> table) {
+    return ImmutableTable.copyOf(table);
+  }
+
+  @Encoding.Builder
+  static class Builder<R, C, V> {
+    private ImmutableTable<R, C, V> buildValue = ImmutableTable.of();
+
+    @Encoding.Init
+    @Encoding.Copy
+    public void set(Table<? extends R, ? extends C, ? extends V> table) {
+      this.buildValue = ImmutableTable.copyOf(table);
+    }
+
+    @Encoding.Build
+    ImmutableTable<R, C, V> build() {
+      return buildValue;
+    }
+  }
 }
-*/
--- encoding-use/src/uses/UseTable.java
+++ encoding-use/src/uses/UseTable.java
@@ -1,9 +1,12 @@
 package uses;
 
+import com.google.common.collect.Table;
 import com.google.common.collect.ImmutableTable;
 import org.immutables.value.Value;
 
 @Value.Immutable
-interface UseTable {
-  ImmutableTable<String, String, String> values();
+interface UseTable<V> {
+  ImmutableTable<String, String, V> values();
+
+  Table<Integer, Integer, V> intValues();
 }
